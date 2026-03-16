001ba8947059825c8cb789264214c44175be28c2
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-03-16 12:04:43 +0100

    complete exercise without css

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -10,12 +10,22 @@ export default function AppMain() {
         { title: 'Pulp Fiction', genre: 'Thriller' },
     ]
 
-    const [selectGenre, setSelectGenre]=useState("")
+    const [selectGenre, setSelectGenre]=useState("intera lista")
+    const [originalArray, setOriginalArray]=useState(films)
+
+    useEffect(()=>{
+        if(selectGenre==="intera lista"){
+            setOriginalArray(films)
+        }else{
+            const filteredFilms= films.filter(film=> film.genre===selectGenre)
+            setOriginalArray(filteredFilms)
+        }
+    },[selectGenre])
 
     return (
         <>
             <select value={selectGenre} onChange={(e)=> setSelectGenre(e.target.value)} class="form-select" aria-label="Default select example">
-                <option selected>Che genere di film vuoi vedere?</option>
+                <option selected value="intera lista">Che genere di film vuoi vedere?</option>
                 <option value="Fantascienza">Fantascienza</option>
                 <option value="Thriller">Thriller</option>
                 <option value="Romantico">Romantico</option>
@@ -23,7 +33,7 @@ export default function AppMain() {
             </select>
             <ul>
                 {
-                    films.map((film, index) => (
+                    originalArray.map((film, index) => (
                         <li key={index}>TITOLO:{film.title} GENERE:{film.genre}</li>
                     ))
                 }
